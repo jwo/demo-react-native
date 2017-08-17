db80f79d4b19aaa9566d743f9b8befcb63cf52b0
Jesse Wolgamott <jesse@comalproductions.com>
2017-08-17 14:40:04 -0500

    Base config for react-native

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Welcome to the danger zone!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 42,
+    color: "#fff",
+    textAlign: 'center'
+  }
+});
